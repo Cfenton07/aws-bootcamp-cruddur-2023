071084b60f43dfdfaee4389a3bf687df2ca4464d
C Fenton <Chricfenton2013@gmail.com>
2025-06-16 02:08:35 +0000

    added my name to backend replacing andrew and made chages to url in desktop navigation js

--- backend-flask/services/messages.py
+++ backend-flask/services/messages.py
@@ -11,16 +11,16 @@ class Messages:
     results = [
       {
         'uuid': '4e81c06a-db0f-4281-b4cc-98208537772a' ,
-        'display_name': 'Andrew Brown',
-        'handle':  'andrewbrown',
-        'message': 'Cloud is fun!',
+        'display_name': 'Chris Fenton',
+        'handle':  'chrisfenton',
+        'message': 'Cloud is the future!',
         'created_at': now.isoformat()
       },
       {
         'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-        'display_name': 'Andrew Brown',
-        'handle':  'andrewbrown',
-        'message': 'This platform is great!',
+        'display_name': 'Chris Fenton',
+        'handle':  'chrisfenton',
+        'message': 'You have to be as agnostic as possible when architecting in the cloud!',
         'created_at': now.isoformat()
     }]
     model['data'] = results
