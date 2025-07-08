c7315dd276002d0ec8824da8a96a8bf6e8fac910
C Fenton <Chricfenton2013@gmail.com>
2025-07-08 02:20:41 +0000

    updated the docker compose file for honeycomb and added ports with port name, default to public

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -18,22 +18,22 @@ from services.show_activity import *
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.exporter.otlp.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.trace import TraceProvider
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 #Initialize tracing and an exporter that can send data to Honeycomb ...
-provider = TraceProvider()
+provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
-trace.set_tacer_provider(provider)
+trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 
 #Initialize automatic instrumentation with Flask (Honeycomb)
-FlaskInstrumentor().instrusment_app(app)
-RequestsInstrumentor().instrusment()
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
