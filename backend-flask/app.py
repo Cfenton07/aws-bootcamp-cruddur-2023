5d739dffe6fa2c1120c1d4c10823136a5ab5c62b
C Fenton <Chricfenton2013@gmail.com>
2025-07-17 01:30:23 +0000

    Updated my backend-flask app.py file to place the app middleware recorder in correct placement. updated my env for AWS access secret key based on how I stored these values in my workspace.

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -36,7 +36,7 @@ provider.add_span_processor(processor)
 #X-Ray------ Starting the recorder
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
 
 #Will show in logs within the backend-flask app (STDOUT)
 simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
@@ -46,6 +46,8 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+#X-Ray------ 
+XRayMiddleware(app, xray_recorder)
 
 #Initialize automatic instrumentation with Flask (Honeycomb)
 FlaskInstrumentor().instrument_app(app)
